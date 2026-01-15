4b1e917f6714ea89face7728441c5383870259e3
gndlchn <a11806595@unet.univie.ac.at>
2026-01-15 14:43:22 +0100

    Update script.js

--- charts/bar_chart/script.js
+++ charts/bar_chart/script.js
@@ -1,5 +1,5 @@
 window.renderBarChart = function(containerId, state) {
-  const margin = { top: 50, right: 30, bottom: 50, left: 140 };
+  const margin = { top: 80, right: 30, bottom: 80, left: 180 };
   const totalWidth = 800;
   // const totalHeight = 400;
   // new
